43a5456a4c48ca96df8d5c1f030cb1cae77d1c50
Lucy King <kinglucyann@gmail.com>
2024-04-16 16:43:57 +0100

    'first set up'

--- Server/server.js
+++ Server/server.js
@@ -4,7 +4,6 @@ const { Client, Events, GatewayIntentBits } = require('discord.js');
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
